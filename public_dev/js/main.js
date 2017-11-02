9be2e66b0c4354402965cbb802f4a3043c72d9e2
santosh <santosh_citech@live.in>
2017-11-02 19:19:31 +0530

    llllll

--- public_dev/js/main.js
+++ public_dev/js/main.js
@@ -1,9 +1,9 @@
 function toggleState(item) {
     if (item.className == "btn-circle-on") {
         item.className = "btn-circle-off";
-        document.getElementById("togglebtnId").innerHTML  = "off";
+        document.getElementById("togglebtnId").innerHTML  = "OFF";
     } else {
         item.className = "btn-circle-on";
-        document.getElementById("togglebtnId").innerHTML  = "on";
+        document.getElementById("togglebtnId").innerHTML  = "ON";
     }
 }
