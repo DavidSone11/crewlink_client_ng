f583041da86ab664ae9dc5a8c38560a3e37b36d1
santosh <santosh_citech@live.in>
2017-11-03 15:04:53 +0530

    dasdasd

--- public_dev/ng/controllers/home.js
+++ public_dev/ng/controllers/home.js
@@ -10,15 +10,6 @@ angular.module('crewLinkNgApp')
   .controller('homeCtrl', function ($scope, $position, toaster) {
 
 
-    $scope.showpopup = function () {
-      toaster
-        .pop({
-          type: 'success',
-          title: 'Removed',
-          body: 'home Removed Successfully!!!'
-        }); 
-
-    };
-
+     
 
   });
