85c476bbd87025b2957da51a9e08b5978d9c2b17
santosh <santosh_citech@live.in>
2017-11-06 00:36:21 +0530

    dasdasda

--- public_dev/ng/controllers/userchart.controller.js
+++ public_dev/ng/controllers/userchart.controller.js
@@ -40,6 +40,42 @@ angular.module('crewLinkNgApp')
 
         };
 
+        $scope.donut = {
+            labels: ["Download Sales", "In-Store Sales", "Mail-Order Sales"],
+            data: [300, 500, 100],
+            options: { legend: { display: true } },
+        };
+
+        $scope.radar = {
+            labels: ["Eating", "Drinking", "Sleeping", "Designing", "Coding", "Cycling", "Running"],
+
+            data: [
+                [65, 59, 90, 81, 56, 55, 40],
+                [28, 48, 40, 19, 96, 27, 100]
+            ]
+        };
+
+        $scope.pie = {
+            labels: ["Download Sales", "In-Store Sales", "Mail-Order Sales"],
+            data: [300, 500, 100]
+        };
+
+        $scope.polar = {
+            labels: ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"],
+            data: [300, 500, 100, 40, 120]
+        };
+
+        $scope.dynamic = {
+            labels: ["Download Sales", "In-Store Sales", "Mail-Order Sales", "Tele Sales", "Corporate Sales"],
+            data: [300, 500, 100, 40, 120],
+            type: 'PolarArea',
+
+            toggle: function () {
+                this.type = this.type === 'PolarArea' ?
+                    'Pie' : 'PolarArea';
+            }
+        };
+
 
 
     });
