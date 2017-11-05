687ebcbb3102f1ddd3492a6117b579cf22af4b49
santosh <santosh_citech@live.in>
2017-11-06 00:30:33 +0530

    dasda

--- public_dev/ng/controllers/userchart.controller.js
+++ public_dev/ng/controllers/userchart.controller.js
@@ -7,16 +7,16 @@
  * Controller of the crewLinkNgApp
  */
 angular.module('crewLinkNgApp')
-  .controller('UserChartController', function ($scope, toaster, $timeout) {
+    .controller('UserChartController', function ($scope, toaster, $timeout) {
 
 
-      console.log("user chart contoller");
+        console.log("user chart contoller");
 
-      
+
         $scope.line = {
             labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
             series: ['Series A', 'Series B'],
-            options : { legend: { display: true }},
+            options: { legend: { display: true } },
             data: [
                 [65, 59, 80, 81, 56, 55, 40],
                 [28, 48, 40, 19, 86, 27, 90]
@@ -25,7 +25,21 @@ angular.module('crewLinkNgApp')
                 console.log(points, evt);
             }
         };
-    
-     
 
-  });
+        $scope.bar = {
+            labels: ['2006', '2007', '2008', '2009', '2010', '2011', '2012'],
+            series: ['Series A', 'Series B'],
+            options: { legend: { display: true } },
+            data: [
+                [65, 59, 80, 81, 56, 55, 40],
+                [28, 48, 40, 19, 86, 27, 90]
+            ],
+            onClick: function (points, evt) {
+                console.log(points, evt);
+            }
+
+        };
+
+
+
+    });
