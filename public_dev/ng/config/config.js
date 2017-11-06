36cea619ada394c707b71a2091050957ca8eef2a
santosh <santosh_citech@live.in>
2017-11-06 18:16:11 +0530

    dasdasd

---
+++ public_dev/ng/config/config.js
@@ -0,0 +1,14 @@
+/*==========================================================
+    Author      : Santosh
+    Date Created: 6 Nov 2017
+    Description : Global configuration defined here
+ ===========================================================*/
+
+ var appConfig = {
+    title: "Corporate Directory",
+    lang: "en",
+    dateFormat: "mm/dd/yy",
+    apiBase: 'http://192.168.168.213:3000/api/',
+    theme: 'skin-purple',
+    layout:""
+};
