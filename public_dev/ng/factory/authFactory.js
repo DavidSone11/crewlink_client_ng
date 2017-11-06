0d3825afedf80dc17071888e0bf50eda04a11eaa
santosh <santosh_citech@live.in>
2017-11-06 20:30:02 +0530

    dasdsad

---
+++ public_dev/ng/factory/authFactory.js
@@ -0,0 +1,18 @@
+' use strict';
+/**
+ * Author      : Santosh
+   Date Created: 6 NOV 17
+   Description : To handle the Factory for Login module
+ */
+
+
+var app = angular.module('crewLinkNgApp');
+app.factory('AuthenticateFactory', ['$http', '$q', '$state', '$cookies', '$window', '$location', function ($http, $q, $state, $window, $cookies, $location) {
+
+    var isLoggedIn
+
+    return {
+
+    }
+
+}]);
