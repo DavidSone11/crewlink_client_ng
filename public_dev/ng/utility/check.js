00e5babba2ac3672c8d3ad69aebd965d0b3f447e
santosh <santosh_citech@live.in>
2017-11-14 22:57:46 +0530

    dasdasd

---
+++ public_dev/ng/utility/check.js
@@ -0,0 +1,27 @@
+
+
+
+(function(){
+    ' use strict'
+  //  var a = b = 3;
+  var c = 10;
+
+  var window = {
+    sessionStorage: {
+        token:'',
+        user:'',
+        role:''
+    }
+  }; 
+  window.sessionStorage.token = "74h85665225olpp";
+  window.sessionStorage.user = "SANTOSH"; 
+  window.sessionStorage.role = "ADMIN"; 
+
+
+
+})();
+
+//console.log("a defined ?"+(typeof a!=='undefined'));
+//console.log("b defined ?"+(typeof b!=='undefined'));
+console.log("C = ="+c);
+console.log("C = ="+window);
